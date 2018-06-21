1d5fe896010ae60644739e606484f25d707e0919
‘Brittany <brittanystubbs@Brittanys-MacBook-Pro.local>
2018-06-21 19:38:04 -0400

    adding third prompt

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,4 +1,5 @@
 alert("Hi!"); 
 let name = prompt("What's your name?");
-let faveColor = prompt("What's your favorite color?"); 
-confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + faveColor + " too.");
+let faveColor = prompt("What's your favorite color?");
+let faveAnimal = prompt("What's your favorite animal?"); 
+confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + faveColor + " too. But I'm allergic to " + faveAnimal + "s.");
--- main.js
+++
@@ -1,4 +0,0 @@
-alert("Hi!"); 
-let name = prompt("What's your name?");
-let faveColor = prompt("What's your favorite color?"); 
-confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + faveColor + " too.");
