c46ad4ceffa86906eb6ef6798d7e4b4d28422927
Harold Wolfinger <harold.b.wolfinger@gmail.com>
2018-06-20 20:33:40 -0400

    Adding index.html, main.css, and the main.js file, which include alert, prompt and confirm functions.

---
+++ lab-01/main.js
@@ -0,0 +1,13 @@
+console.log(alert("Let\'s go"));
+
+let name = prompt("What\'s your name");
+console.log(name)
+
+let origin = prompt("Hi " + name + ". Where are you from?")
+let continny = confirm(name + " to continue please press OK.")
+let coder = prompt("What type of coder do you want to be?")
+let months = prompt("In how many months do you want to be a " + coder + " by?")
+
+let msgtoyou = alert("Keep up the good work " + name + " from " + origin + ", you will be a " + coder + " in " + months + " months IF you work hard.")
+
+console.log(origin)
