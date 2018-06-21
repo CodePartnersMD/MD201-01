095be100dde5229febc07af911994bc13ae64c52
‘Brittany <brittanystubbs@Brittanys-MacBook-Pro.local>
2018-06-21 19:46:00 -0400

    added last question

--- lab-01/main.js
+++ lab-01/main.js
@@ -2,4 +2,5 @@ alert("Hi!");
 let name = prompt("What's your name?");
 let faveColor = prompt("What's your favorite color?");
 let faveAnimal = prompt("What's your favorite animal?"); 
-confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + faveColor + " too. But I'm allergic to " + faveAnimal + "s.");
+let faveBand = prompt("What's your favorite band?");
+confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + faveColor + " too. But I'm allergic to " + faveAnimal + "s. Did you hear that " + faveBand + " is in town next week?");
