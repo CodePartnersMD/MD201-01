09a95e24fe1c88c9b35bd9ff2791f54a537cde34
Adrian <adrianh.macairan@gmail.com>
2018-06-23 10:12:50 -0400

    extendedhw

---
+++ lab-01/main.js
@@ -0,0 +1,8 @@
+let name = prompt("What's your name?");
+let origin = prompt('Where are you from?')
+let coder = prompt('Are you a coder?')
+
+console.log(name)
+console.log(origin)
+console.log(coder)
+
