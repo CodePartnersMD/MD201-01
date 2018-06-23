9d61ef53428f2121447cb10a286dcaa095f7d894
Jalaluddin Kasaat <jalaluddinkasaat@MacBook-Pro.local>
2018-06-23 13:38:47 -0400

    added prompts

---
+++ main.js
@@ -0,0 +1,8 @@
+let problem = prompt("How big a health problem is malaria?");
+let countries = prompt("What countries have the highest malaria cases?");
+let cases = prompt("How many cases of malaria are recoreded worldwide each year?");
+let casesAfg = prompt("How may malaria cases were recorded in 2016 in Afghanistan?");
+console.log(problem);
+console.log(countries);
+console.log(cases);
+console.log(casesAfg);
