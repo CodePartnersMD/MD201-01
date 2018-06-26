5c245506479c3bbb6ec21ee18973601e675432aa
NAME <EMAIL>
2018-06-26 17:40:40 -0400

    Added file to repo for submission

---
+++ Lab-1/main.js
@@ -0,0 +1,12 @@
+//Author:Ishmael Sunday | github.com/okayishmael
+//Code 201 Project1 @ Code Partners > https://codepartners.net
+
+
+let name = prompt("What\'s your name?"); //declare var use prompt to display value in browser and collect user input
+if (name != null){
+alert("Hi "+name+"! Please click the \"OK button\" to enter your current city.")}; // Same here
+let origin = prompt("Enter your current City:");
+let gender = prompt("Gender:");
+let coder = confirm('Click "OK" if you are a coder.'); // declare var confirm to collect user input
+console.log(); // Conprint
+
