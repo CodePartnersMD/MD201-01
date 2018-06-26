a8fcedc0f4b5b2a48d0ef528aa14e44663a2e211
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-06-26 12:40:26 -0400

    changed

---
+++ main.js
@@ -0,0 +1,6 @@
+let name = prompt("What is your name?");
+let origin = prompt("Where are you from?");
+let coder = prompt("Are you a coder?");
+console.log(name);
+console.log(origin);
+console.log(coder);
