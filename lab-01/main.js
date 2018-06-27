af76463be1993dc9ad3d94dbac2777f54fb7784d
NAME <EMAIL>
2018-06-27 18:07:24 -0400

    added lab 2

---
+++ lab-01/main.js
@@ -0,0 +1,8 @@
+// let name = prompt("What\'s your name");
+// let origin = prompt('Where are you from?')
+let coder = confirm('are you a coder?')
+
+console.log(coder)
+// console.log(name);
+// console.log(origin)
+
