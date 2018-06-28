ebf13d4148458fa74542960390469e4d905dc18a
Adrian <adrianh.macairan@gmail.com>
2018-06-28 18:27:30 -0400

    Extended About me

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,8 +1,42 @@
-let name = prompt("What's your name?");
-let origin = prompt('Where are you from?')
-let coder = prompt('Are you a coder?')
+console.log(alert("Let's go"));
 
-console.log(name)
-console.log(origin)
-console.log(coder)
+//These are the prompts section (pop-ups)
+let name = prompt("What's your name?");
+let movies = prompt('Do you like movies?');
+let coder = prompt('Are you a coder?');
+let faveNum = prompt('What is your favorite number?');
+let sports = prompt('Do you play sports?');
+let born = prompt('Are you born in the United States?');
+
+//The "if" functions section
+if(name){
+    document.getElementById('name').innerText = name;
+}
+
+if(movies){
+    document.getElementById('movies').innerText = movies;
+}
+
+if(coder){
+    document.getElementById('coder').innerText = coder;
+}
+
+if(faveNum){
+    document.getElementById('faveNum').innerText = faveNum;
+}
+
+if(sports){
+    document.getElementById('sports').innerText = sports;
+}
+
+if(born){
+    document.getElementById('born').innerText = born;
+}
+
+//This section is where I put my console.log(s)
+console.log(name);
+console.log(movies);
+console.log(coder);
+console.log(faveNum); 
+console.log(born);
 
