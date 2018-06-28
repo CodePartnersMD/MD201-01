d58b59181c7c12246f1d7ed2b6d1324b46205642
Patrice Thomas <patrice.thomas1229@gmail.com>
2018-06-28 18:41:34 -0400

    added array and css

---
+++ lab-01/main.js
@@ -0,0 +1,26 @@
+let name= prompt('What\s your name');
+name = 'i am ' + name;
+console.log(name)
+
+let age= prompt ('How old are you?');
+age = age +' years old';
+console.log(age);
+
+let continent= prompt('what continent are you from?');
+continent = 'from '+ continent;
+console.log(continent);
+
+let greetings = confirm(name + " " + age + " " +continent);
+console.log(greetings);
+
+
+if(name){
+    document.getElementById('Name-Of-User').innerText = name
+};
+
+if(age){
+    document.getElementById('Your-Age').innerText = age
+};
+if(continent){
+    document.getElementById('Birth-Place').innerText = continent
+};
