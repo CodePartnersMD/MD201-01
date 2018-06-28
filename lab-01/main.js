2cad88086bc2991ad0f3c3715777e3dc81bf9322
‘Brittany <brittanystubbs@Brittanys-MacBook-Pro.local>
2018-06-28 19:28:50 -0400

    fixed

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,10 +1,59 @@
+let captchaArray = ["erFY4", "br9kl", "qwbM7", "DejK8", "CDxz8", "wefk3", "qwkl8", "Pgh2m", "Wfz5m", "Tgh3M"];
+let captcha = captchaArray[Math.floor(Math.random() * captchaArray.length)];
+
 alert("Hi!"); 
-let name = prompt("What's your name?");
-console.log(name);
-let faveColor = prompt("What's your favorite color?");
-console.log(prompt);
-let faveAnimal = prompt("What's your favorite animal?"); 
-console.log(faveAnimal);
-let faveBand = prompt("What's your favorite band?");
-console.log(faveBand);
-confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + faveColor + " too. But I'm allergic to " + faveAnimal + "s. Did you hear that " + faveBand + " is in town next week?");
+let captchaResponse = prompt("First, let's make sure you're human. Please type the following (case sensitive): " + captcha);
+
+if (captcha == captchaResponse)
+{
+    let name = prompt("What's your name?");
+    console.log(name);
+    let faveColor = prompt("What's your favorite color?");
+    console.log(prompt);
+    let faveAnimal = prompt("What's your favorite animal?"); 
+    console.log(faveAnimal);
+    let faveBand = prompt("What's your favorite band?");
+    console.log(faveBand);
+    confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + faveColor + " too. But I'm allergic to " + faveAnimal + "s. Did you hear that " + faveBand + " is in town next week?");
+    alert("What are your top three favorite movies?")
+    let firstMovie = prompt("First favorite movie");
+    console.log(firstMovie);
+    let secondMovie = prompt("Second favorite movie");
+    console.log(secondMovie);
+    let thirdMovie = prompt("Third favorite movie");
+    console.log(thirdMovie);
+    confirm("Oh, I love " + firstMovie + "! But I haven't seen " + secondMovie + " or " + thirdMovie + " though.");
+
+    if(name!=null){
+        document.getElementById('name').innerText = name
+    }
+    
+    if(faveColor!=null){
+        document.getElementById('faveColor').innerText = faveColor
+    }
+    
+    if(faveAnimal!=null){
+        document.getElementById('faveAnimal').innerText = faveAnimal
+    }
+    
+    if(faveBand!=null){
+        document.getElementById('faveBand').innerText = faveBand
+    }
+    
+    if(firstMovie!=null){
+        document.getElementById('firstMovie').innerText = firstMovie
+    }
+    
+    if(secondMovie!=null){
+        document.getElementById('secondMovie').innerText = secondMovie
+    }
+    
+    if(thirdMovie!=null){
+        document.getElementById('thirdMovie').innerText = thirdMovie
+    }
+}
+
+else{
+    let err = confirm("Sorry, incorrect response. Please reload the page.")
+}
+
