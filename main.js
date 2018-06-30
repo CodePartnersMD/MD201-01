478822d6fe1c84781b5cceaf8483502b2c4e4d01
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-06-30 16:56:09 -0400

    feedback to Jalal

--- main.js
+++ main.js
@@ -1,47 +1,38 @@
+let captchaArray = ["CC25l", "DDeF9", "qRSTu9", "55JjK", "TTl09", "66iUJ", "rSTU8", "Pgh2m", "Wfz5m", "werP8", "98EezJ","ZzYu6"];
+let captcha = captchaArray[Math.floor(Math.random() * captchaArray.length)];
 
-let name = prompt("What's your name?");
-let problem = prompt("Thanks, " +name+ ". How big a health problem is malaria?");
-let country = prompt("What country do you think has the highest malaria cases?");
-let cases = prompt("How many cases of malaria are recored each year in " +country+ "?");
-let ending = alert("Thanks, " +name+ ", for your answers.")
-console.log (name, problem, country, cases)
+alert("We would appreciate if you can take this survey"); 
+let captchaResponse = prompt("Let’s check you are a human. Please type this below: "+ captcha);
 
-// console.log(problem);
-// console.log(country);
-// console.log(cases);
+if (captcha == captchaResponse){
 
+   let name = prompt("What\’s your name?");
+   let origin = prompt("Thanks, " + name + ". Where are you from?");
+   console.log(name);
+   console.log(origin)
 
+   let malariaCountries = prompt(name + ", can you tell me where malaria is so common?");
 
+   malariaCountries = ["Afghanistan", "Nigeria", "Uganda"];
 
+   for (let i=0; i<malariaCountries.length; i++) {
 
-// let captcha = prompt("If you are a human, type this word:" + human);
-// let human = ["car", "bike", "motorbike"];
+   confirm("Is" +malariaCountries[i]+ "one of the malaria countries?");
+   console.log(malariaCountries)
 
-// for (let i=0; i<human.length; i++){
+   };
 
-//     console.log(human[i])
-// };
+}
 
+else{
+   let notHuman = alert("please reload the page and try again.");
+   console.log(notHuman)
+};
 
+let healthStat = ("Afghanistan has one physician for every 3,750 people")
 
-// let name = prompt("What\'s your name?");
-// let origin = prompt("Thanks, " + name + ". Where are you from?");
+if(healthStat) {
+   document.getElementById("malaria-stat").innerText = healthStat
+   console.log(healthstat)
 
-// let malariaCountries = prompt(name + ", can you tell me where malaria is so common?");
-
-// malariaCountries = ["Afghanistan", "Nigeria", "Uganda"];
-
-// for (let i=0; i<malariaCountries.length; i++) {
-
-//     console.log(malariaCountries[i])
-// };
-
-
-// let captcharesponse = prompt(first, let me know.  please type the followng" + captcha)
-// if (captcha == captchaResponse)
-
-// console.log(name);
-// console.log(origin);
-// console.log(malariaCountries);
-// console.log(human)
-// console.log(captcha)
+};
