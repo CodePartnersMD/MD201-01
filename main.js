5d90fbb31db0a4717376db54cd117f79862e22d9
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-06-30 15:18:00 -0400

    submit lab-2

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-let name = prompt("What is your name?");
-let origin = prompt("Where are you from?");
-let coder = prompt("Are you a coder?");
+let name = prompt("What\'s your name?");
 console.log(name);
-console.log(origin);
-console.log(coder);
+let human = ['AhO42', 'Gkr6p', 'TtQy5', 'SjvL2'];
+let random = Math.floor(Math.random() * 3);
+let test = human[random];
+let humanCheck = prompt('Type in the following word to prove you are human ' + name + " : " + test)
+let greeting = confirm("Hi " + name + " nice to meet you" );
+let resident = confirm("Are you a lawful resident of US " + name +"?");
+let date = prompt("When did you enter to US " + name + "?");
+let myLocation = prompt("Which airport did you use it, when you enter to Us on " + date +" "+ name + " ?");
+console.log(greeting);
+console.log(resident);
+console.log(date);
+console.log(myLocation);
+console.log(alert("Let\'s go " + name));
+let myArray= [' White', 'Hispanic or Latino', 'African-American' ,'East Asia']
+let captcha = prompt('Please write your race ' + myArray)
+
+var country = ["Iran" , "Canada" , "Egipt"]
+for (var i=0; i< country.length ; i++){
+    confirm ("Is your country? " + country[i]);
+}
+if(name != null){
+    document.getElementById('name').innerText = name
+}
