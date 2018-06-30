ca56fe8e54e19a9761e72af30c35780f98cf293e
Patrice Thomas <patrice.thomas1229@gmail.com>
2018-06-30 13:34:50 -0400

    New Changes to CSS

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,3 +1,5 @@
+//creating prompts for user to input value and storing those values using console.log
+
 let name= prompt('What\s your name');
 name =  name;
 console.log(name)
@@ -48,4 +50,17 @@ if(emptyArray[0]==true && emptyArray[1] == true && emptyArray[2]== true ){
     alert('I\'m sorry, I can\'t verify your humaneness')
 };
 
-//Ask Zach to show the counter loop in Javascript
+/*Ask Zach to show the counter loop in Javascript
+
+let count = 0; 
+
+for(let 1 =0; 1 < areYouHuman.length; i++) {
+    checkHuman = confirm( 'Do you" + 'likr soccer)
+    if (checkHuman)  {
+        count++
+    }
+}if (count= areYouHuman.length) {
+    alert('Welcome Human')
+} else {
+    alert ('I'm sorry, I can't verify your humaneness)
+}
