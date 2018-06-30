374c6e8a4c4f7061235ed7df2d43fc3ddd362694
Harold Wolfinger <haroldwolfinger@protonmail.com>
2018-06-30 16:09:38 -0400

    BRITTANY's COMPLETELY RANDOMIXED CAPTCHA, it's really really cool.  Plus, eliminated the if-else statement at the end of the game since it was unnecessary.

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,10 +1,21 @@
 // This code includes everything uploaded to gh-pages, not what was on the origin branch that was originally cloned -hw
 
-let captchaArray = ["erFY4", "br9kl", "qwbM7", "DejK8", "CDxz8", "wefk3", "qwkl8", "Pgh2m", "Wfz5m", "Tgh3M"];
-let captcha = captchaArray[Math.floor(Math.random() * captchaArray.length)];
+let captchaArray = ['A','B','C','D','E','F','G','H','J','K','L','M','N', 'P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z','2', '3', '4', '5', '6', '7', '8', '9']
+
+let captcha = captchaArray[Math.floor(Math.random() * captchaArray.length)] + captchaArray[Math.floor(Math.random() * captchaArray.length)] + captchaArray[Math.floor(Math.random() * captchaArray.length)] + captchaArray[Math.floor(Math.random() * captchaArray.length)] + captchaArray[Math.floor(Math.random() * captchaArray.length)]+ captchaArray[Math.floor(Math.random() * captchaArray.length)];
+
+// let captchaArray = ["erFY4", "br9kl", "qwbM7", "DejK8", "CDxz8", "wefk3", "qwkl8", "Pgh2m", "Wfz5m", "Tgh3M"];
+// let captcha = captchaArray[Math.floor(Math.random() * captchaArray.length)];
 
 alert("Hi!"); 
+
+// Check captcha, if incorrect ask user to try again
 let captchaResponse = prompt("First, let's make sure you're human. Please type the following (case sensitive): " + captcha);
+console.log("Initial ask: " + captchaResponse);
+while (captchaResponse != captcha) {
+    captchaResponse = prompt("Incorrect response. Please try again. Please type the following (case sensitive): " + captcha);
+    console.log("New user captcha response: " + captchaResponse)
+}
 
 let nameFunction = function() {
     return prompt("What's your name?");
@@ -34,8 +45,6 @@ let movie3Function = function () {
     return prompt("Third favorite movie");
 }
 
-if (captcha == captchaResponse)
-{
     //Brittany's updated code for NAME that is wrapped in a function
     let name = nameFunction()
     console.log(name)
@@ -70,6 +79,7 @@ if (captcha == captchaResponse)
 
 
     confirm("Hi, "+ name + "! It's nice to meet you. My favorite color is " + color + " too. But I'm allergic to " + animal + "s. Did you hear that " + band + " is in town next week?");
+    
     alert("What are your top three favorite movies?")
     
 
@@ -96,35 +106,30 @@ if (captcha == captchaResponse)
     
     confirm("Oh, I love " + firstMovie + "! But I haven't seen " + secondMovie + " or " + thirdMovie + " though.");
 
-    if(name){
-        document.getElementById('name').innerText = name
-    }
-    
-    if(faveColor){
-        document.getElementById('faveColor').innerText = color
-    }
-    
-    if(faveAnimal){
-        document.getElementById('faveAnimal').innerText = animal
-    }
-    
-    if(faveBand){
-        document.getElementById('faveBand').innerText = band
-    }
-    
-    if(firstMovie){
-        document.getElementById('firstMovie').innerText = firstMovie
-    }
-    
-    if(secondMovie){
-        document.getElementById('secondMovie').innerText = secondMovie
-    }
-    
-    if(thirdMovie){
-        document.getElementById('thirdMovie').innerText = thirdMovie
-    }
+if(name){
+    document.getElementById('name').innerText = name
 }
 
-else{
-    let err = confirm("Sorry, incorrect response. Please reload the page.")
+if(faveColor){
+    document.getElementById('faveColor').innerText = color
+}
+
+if(faveAnimal){
+    document.getElementById('faveAnimal').innerText = animal
+}
+
+if(faveBand){
+    document.getElementById('faveBand').innerText = band
+}
+
+if(firstMovie){
+    document.getElementById('firstMovie').innerText = firstMovie
+}
+
+if(secondMovie){
+    document.getElementById('secondMovie').innerText = secondMovie
+}
+
+if(thirdMovie){
+    document.getElementById('thirdMovie').innerText = thirdMovie
 }
