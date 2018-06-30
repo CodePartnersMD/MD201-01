de4d20200fa8a292ccf1ad35bd4ac6554a035014
NAME <EMAIL>
2018-06-30 12:27:28 -0400

    pushing lab readmes and code example

---
+++ lab-03/lab/functions.js
@@ -0,0 +1,7 @@
+let variableName
+
+let myFunction = function(str) {
+    return 'This is our first function taking ' + str +  'as a parameter'
+}
+
+myFunction('Harold is awesome')
--- lab-03/lab/main.js
+++ lab-03/lab/main.js
@@ -1,19 +1,19 @@
 let myArray = ['like soccer', 'watch soccer', 'root for Brazil']
-// let emptyArray = []
-// let checkHuman
+let emptyArray = []
+let checkHuman
 
-// for(let i = 0; i < myArray.length; i++) {
-//     checkHuman = confirm('Do you ' + myArray[i])
-//     if(checkHuman) {
-//         emptyArray.push(myArray[i])
-//     }
-// }
+for(let i = 0; i < myArray.length; i++) {
+    checkHuman = confirm('Do you ' + myArray[i])
+    if(checkHuman) {
+        emptyArray.push(myArray[i])
+    }
+}
 
-// if(myArray.length === emptyArray.length) {
-//     alert('welcome Human')
-// } else {
-//     alert('sorry robot!')
-// }
+if (myArray.length === emptyArray.length) {
+    alert('welcome Human')
+} else {
+    alert('sorry robot!')
+}
 
 let count = 0
 
