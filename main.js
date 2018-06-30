0c69392f0c1e52685018871dc545af9f8a3234ec
Jalaluddin Kasaat <jalaluddinkasaat@MacBook-Pro.local>
2018-06-30 10:02:51 -0400

    added concantenations

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+let name = prompt("What's your name?");
+let problem = prompt("Thanks, " +name+ ". How big a health problem is malaria?");
+let country = prompt("What country do you think has the highest malaria cases?");
+let cases = prompt("How many cases of malaria are recored each year in " +country+ "?");
+let ending = alert("Thanks, " +name+ ", for your answers.")
+console.log (name, problem, country, cases)
+
+// console.log(problem);
+// console.log(country);
+// console.log(cases);
+
+
+
+
+
+// let captcha = prompt("If you are a human, type this word:" + human);
+// let human = ["car", "bike", "motorbike"];
+
+// for (let i=0; i<human.length; i++){
+
+//     console.log(human[i])
+// };
+
+
+
+// let name = prompt("What\'s your name?");
+// let origin = prompt("Thanks, " + name + ". Where are you from?");
+
+// let malariaCountries = prompt(name + ", can you tell me where malaria is so common?");
+
+// malariaCountries = ["Afghanistan", "Nigeria", "Uganda"];
+
+// for (let i=0; i<malariaCountries.length; i++) {
+
+//     console.log(malariaCountries[i])
+// };
+
+
+// let captcharesponse = prompt(first, let me know.  please type the followng" + captcha)
+// if (captcha == captchaResponse)
+
+// console.log(name);
+// console.log(origin);
+// console.log(malariaCountries);
+// console.log(human)
+// console.log(captcha)
+
+
