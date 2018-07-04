55d19bb7b274838d09f574f468d249b579c2451d
NAME <EMAIL>
2018-07-03 20:24:23 -0400

    pushing code demo for lecture 6

--- lecture-06/lab/main.js
+++ lecture-06/lab/main.js
@@ -1,7 +1,7 @@
 let storeOne = {
     name: 'Chocolate Store',
     product : 'chocolate cookies',
-    customers: 200,
+    customers: 40,
     employees: 3,
     location: 'Bethesda',
     createCustomer: function() {
@@ -20,22 +20,64 @@ let storeTwo = {
     }
 }
 
-let myBody = document.getElementById('myBody')
+//array of objects
+let objArray = [storeOne, storeTwo]
 
-let storeOneTitle = document.createElement('h1')
-myBody.appendChild(storeOneTitle)
-storeOneTitle.setAttribute('class', 'store')
-storeOneTitle.innerText = storeOne.name
+//creating elements
+let elBody = document.getElementById('table-container')
+let elTable = document.createElement('table')
 
-let storeOneLocation = document.createElement('p')
-myBody.appendChild(storeOneLocation)
-storeOneLocation.setAttribute('id', 'store-one-location')
-storeOneLocation.innerText = storeOne.location
+//setting a class to the new table we just created
+elTable.setAttribute('id', 'id-table')
 
-let storeOneCustomers = document.createElement('p')
-myBody.appendChild(storeOneCustomers)
-storeOneCustomers.innerText = `# of customers    ${storeOne.customers} in ${storeOne.location}`
-'# of customers ' + storeOne.customers 
+//appending our table to the body
+elBody.appendChild(elTable)
+
+//looping through the array of objects 
+for(let i = 0; i < objArray.length; i++) {
+    let elRow = document.createElement('tr')
+    elTable.appendChild(elRow)
+    let elTh = document.createElement('th')
+    elTh.innerText = objArray[i].name
+    elRow.appendChild(elTh)
+    let product = document.createElement('td')
+    product.innerText = objArray[i].product
+    elRow.appendChild(product)
+    let location = document.createElement('td')
+    location.innerText = objArray[i].location
+    elRow.appendChild(location)
+    
+
+    // console.log(objArray[i].name)
+    // console.log(objArray[i].location)
+    // console.log(objArray[i].employees)
+    // objArray[i].price = 10
+    // objArray[i].revenue = objArray[i].price * objArray[i].customers
+    // console.log(objArray[i].revenue)
+    // console.log(objArray[i])
+}
+
+
+// storeTwo.hours = '9-5'
+
+// let myBody = document.getElementById('myBody')
+
+// let storeOneTitle = document.createElement('h1')
+// myBody.appendChild(storeOneTitle)
+// storeOneTitle.setAttribute('class', 'store')
+// storeOneTitle.innerText = storeOne.name
+
+// let storeOneLocation = document.createElement('p')
+// myBody.appendChild(storeOneLocation)
+// storeOneLocation.setAttribute('id', 'store-one-location')
+// storeOneLocation.innerText = storeOne.location
+
+// let storeOneCustomers = document.createElement('p')
+// myBody.appendChild(storeOneCustomers)
+// storeOneCustomers.innerText = `# of customers    ${storeOne.customers} in ${storeOne.location}`
+// '# of customers ' + storeOne.customers 
+
+
 
 
 
