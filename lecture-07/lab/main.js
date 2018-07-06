d542b8b07298ea5599d4db25fdb67a7740f80b57
NAME <EMAIL>
2018-07-06 14:48:57 -0400

    added readme for lecture 8

--- lecture-07/lab/main.js
+++ lecture-07/lab/main.js
@@ -1,12 +1,17 @@
 //creating a constructor function, (template, blueprint)
-let Store = function(name, location, customers){
+let Store = function(name, location, customers, price, cookiesSold){
     this.name = name;
     this.location = location;
     this.customers = customers;
+    this.price = price;
+    this.cookiesSold = cookiesSold
+    this.getRevenue = function() {
+        this.revenue = this.price * this.cookiesSold
+    }
 };
 
 let storeArray = []
-let storeOne = new Store('Chocolate Store', 'Bethesda', 4)
+let storeOne = new Store('Chocolate Store', 'Bethesda', 4, 5, 20)
 let storeTwo = new Store('Vanilla Store', 'Baltimore', 6)
 
 storeArray.push(storeOne)
