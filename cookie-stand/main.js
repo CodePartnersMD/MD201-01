b135ad0e10d98e3e422c7af5153019d818344944
Adrian <adrianh.macairan@gmail.com>
2018-07-12 20:48:46 -0400

    july 12 lab

--- cookie-stand/main.js
+++ cookie-stand/main.js
@@ -8,34 +8,62 @@ function Store(name, product, location, supplies, sold) {
     
     this.checkSales = function() {
         return this.supplies - this.sold;
+        console.log(checkSales);
     };
 };
 
+//getting element of form from html
+let elStore = document.getElementById('storeInfo');
+
+//form user variables
+let userStoreName = elStore.storeName;
+let userStoreProduct = elStore.storeProduct;
+let userStoreLoc = elStore.storeLoc;
+let userStoreSup = elStore.storeSup;
+let userStoreSold = elStore.storeSold;
+
+//creating a table
+let elBody = document.getElementById('newBody');
+let elTable = document.createElement('table');
+elBody.appendChild(elTable);
+let elRow = document.createElement('tr');
+elTable.appendChild(elRow);
+let elTh = document.createElement('th');
+
+elRow.appendChild(elTh);
+elTh.innerText = userStoreName.value;
+
+let elTd = document.createElement('td');
+elRow.appendChild(elTd);
+elTd.innerText = userStoreProduct.value;
+
+let elTd2 = document.createElement('td');
+elRow.appendChild(elTd2);
+elTd.innerText = userStoreLoc.value;
+
+let elTd3 = document.createElement('td');
+elRow.appendChild(elTd3);
+elTd.innerText = userStoreSup.value;
+
 //Store Array
 let storeArray = [];
-
-//getting element of form from html
-let form = document.getElementById('storeInfo');
 
 //Instances
 let vanCookie = new Store ('Vanilla Store', 'Vanilla Cookies', 'Baltimore', 120, 58);
 storeArray.push(vanCookie);
 let choCookie = new Store ('Chocolate Store', 'Chocolate Cookies', 'Bethesda', 170, 110);
 storeArray.push(choCookie);
-
-//form user variables
-let userStoreName = form.storeName;
-let userStoreProduct = form.storeProduct;
-let userStoreLoc = form.storeLoc;
-let userStoreSup = form.storeSup;
-let userStoreSold = form.storeSold;
+let cashCookie = new Store ('Cashew Store', 'Cashew Cookies', 'Rockville', 160, 90);
+storeArray.push(cashCookie);
 
 //addEventListener
-form.addEventListener('submit', function(event) {
+elStore.addEventListener('submit', function(event) {
     event.preventDefault()
     let newStore = new Store(userStoreName.value, userStoreProduct.value, userStoreLoc.value, userStoreSup.value, userStoreSold.value);
     storeArray.push(newStore);
     console.log(storeArray);
+    console.log(newStore);
 });
 
 console.log(storeArray);
+
