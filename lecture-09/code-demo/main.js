4087c49c76e2017e75e2b734da83070919f77a69
NAME <EMAIL>
2018-07-16 08:14:35 -0400

    added overview and pushing code demos

--- lecture-09/code-demo/main.js
+++ lecture-09/code-demo/main.js
@@ -6,11 +6,42 @@ let Wizard = function(name, age, house, pet) {
     this.pet = pet;
 }
 
+//creating an array of class names that will act as my Table header and which I will iterate through to create values for each Wizard
+let classArray = ['Dark Magic', 'Potions', 'Astronomy', 'Beasts and Creatures']
+
+//creating an empty array that will contain my new wizards that I create
 let wizardArray = []
 
+//creating new instances of the Wizard object
+let wizardOne = new Wizard('Harry', 15, 'Gryffindor', 'Toad')
+let wizardTwo = new Wizard('Ron', 15, 'Gryffindor', 'Owl')
+
+//pushing my new wizards into the wizard array
+wizardArray.push(wizardOne)
+wizardArray.push(wizardTwo)
+
+//finding the body in HTML by ID
 let myBody = document.getElementById('main-content')
+//creating a new table
 let newTable = document.createElement('table')
+//apending the table to the body
 myBody.appendChild(newTable)
+
+//looping through the wizard array to populate a row for each wizard that has been created
+for(let i=0; i < wizardArray.length; i++) {
+    let newRow = document.createElement('tr')
+    newTable.appendChild(newRow)
+    let newTh = document.createElement('th')
+    newRow.appendChild(newTh)
+    newTh.textContent = wizardArray[i].name
+    //looping through the class array and generating a random number then displaying that number in a new Td
+    for(let j=0; j < classArray.length; j++) {
+        let sortingNumber = Math.floor(Math.random() * 10)
+        let newTd = document.createElement('td')
+        newRow.appendChild(newTd)
+        newTd.textContent = sortingNumber
+    }
+}
 
 
 let form = document.getElementById('hogwarts-form')
@@ -26,7 +57,6 @@ let userRadio = form.wizardPet
 
 function renderWizards() {
     let newRow = document.createElement('tr')
-    newRow.setAttribute('class', 'wizardRow')
     newTable.appendChild(newRow)
     let newTh = document.createElement('th')
     newRow.appendChild(newTh)
@@ -45,8 +75,6 @@ function addAges() {
 
 function renderAges() {
     let wizardRows = document.getElementsByClassName('wizardRow')
-    // let newFooter = document.createElement('th')
-    // console.log(wizardRows)
     age = addAges()
     newTable.appendChild(footer)
     total.textContent = age
@@ -61,6 +89,7 @@ function submitHandler(event) {
 }
 
 form.addEventListener('submit', submitHandler)
+
 
 
 
